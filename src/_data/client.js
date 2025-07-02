6cf353f894a98eaf7a0bf2220d7203c60c0560c8
Adriel Ramirez <adrielscode@gmail.com>
2025-07-02 18:15:08 -0400

    Transform template into NexSite web development business site

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,11 +1,12 @@
 module.exports = {
     name: "NexSite",
+    tagline: "Modern Web Development Solutions",
     email: "hello@nexsite.com",
     phoneForTel: "555-779-4407",
     phoneFormatted: "(555) 779-4407",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
+        lineOne: "Denver Tech Center",
+        lineTwo: "Suite 200",
         city: "Denver",
         state: "CO",
         zip: "80206",
@@ -13,11 +14,14 @@ module.exports = {
         mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/nexsite",
+        instagram: "https://www.instagram.com/nexsite",
+        twitter: "https://twitter.com/nexsite",
+        linkedin: "https://linkedin.com/company/nexsite",
+        github: "https://github.com/nexsite",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.nexsite.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
